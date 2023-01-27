4b49817bb55579eda8f0f5187a03a4782c3c9a51
Sergio Oseko <osk.sergio@gmail.com>
2023-01-27 04:26:51 -0300

    test: criação da classe Card e melhoria dos testes

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,20 @@
-RSpec.describe 'Card' do
-  it 'has a type' do
+class Card
+  attr_reader :rank, :suit
 
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+end
+
+RSpec.describe Card do
+  it 'has a rank' do
+    card = Card.new('Ace', 'Spades')
+    expect(card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    card = Card.new('Ace', 'Spades')
+    expect(card.suit).to eq('Spades')
   end
 end
